d9c284c356f9c1c3bb15d9cdef87f26f9bcd73dc
Dobbs <benjamindobbs@gmail.com>
2021-02-02 09:21:00 -0500

    Added music functionality

--- puzzles/src/App.vue
+++ puzzles/src/App.vue
@@ -37,11 +37,24 @@
           </v-container>
       
     </v-main>
-    
+    <!-- <v-container justify-center>
+    <v-btn
+          elevation=5
+          color="cyan lighten-1"
+          large
+          class="mr-4"
+    @click="stopSound(this.currentSong)"
+    >
+    Pause Music
+    </v-btn>
+    </v-container> -->
   </v-app>
 </template>
 
 <script>
+const lobby = require("@/assets/lobby.mp3");
+const game =require("@/assets/game.mp3");
+const woosh=require("@/assets/woosh.wav");
 import InputForm from '@/components/InputForm.vue'
 import Timer from '@/components/Timer.vue'
 export default {
@@ -52,19 +65,58 @@ export default {
     questions: ["Password", "What is the Square Root of 169","Capital of Djibouti", "Avatar Aangs Sky Bison",  "What are the Coordinates of Jackson, Wyoming ", "What Year Did Rihanna Release 'Rude Boy'?", "Small Island Nation inbetween Madagascar and Mozambique beggining with the Letter 'C'", "ببغاء في اللغة الألمانية", "Distance from Earth to the Moon in Furlongs", "American Soap Opera premiering October 12th, 1953",  ],
     hints:["Today's Password","Whole Number", "Name of City", "One Word", "Format: 00.0, 00.0", "2XXX", "One Word", "Hint: Translate", "Round to Whole Number", "Two Words"],
     finish: false,
+    lobbyMusic: new Audio(lobby),
+    gameMusic: new Audio(game),
+    currentSong: new Audio(lobby),
+    wooshTrans: new Audio(woosh),
   }),
   components: {
     InputForm,
     Timer,
   },
+  created: function() {
+      this.playSound(this.lobbyMusic);
+      this.currentSong = this.lobbyMusic;
+      this.lobbyMusic.loop = true;
+      this.gameMusic.loop = true;
+      this.currentSong.loop = true;
+  },
   methods: {
     startGame(){
       this.stage = this.stage + 1
+      if (this.stage < 2) {
+      this.stopSound(this.lobbyMusic)
+      this.playSound(this.wooshTrans)
+      setTimeout(() =>  {
+        this.playSound(this.gameMusic);
+      },1500)
+      this.currentSong = this.gameMusic
+      }
+
+
     },
     timeUp(){
       this.finish=!this.finish
+      this.stopSound(this.gameMusic)
+      this.playSound(this.wooshTrans)
+      setTimeout(() =>  {
+      this.playSound(this.lobbyMusic);
+      },1500)
+      this.currentSong = this.lobbyMusic
+    },
+    playSound (sound) {
+      if(sound) {
+        var audio = sound;
+        audio.play();
+      }
+    },
+      stopSound (sound) {
+      if(sound) {
+        var audio = sound;
+        audio.pause();
+      }
     }
-  },
-}
+  }
+  }
 </script>
 <style>
